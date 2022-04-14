28a2a71c57b6c49e2fe6433de30e14632506c682
Adstefnum <bauerstevenson@gmail.com>
2022-04-14 22:36:19 +0300

    created posts table and controller and model

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+        public function show(){
+                return view();
+        }
+
+        public function create(){
+                return view();
+        }
+        public function update(){
+                return view();
+        }
+        public function destroy(){
+                return view();
+        }
+}
